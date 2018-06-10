f1b9cc45b28cc518090586d0a392819670366cc4
Gravit8ion <root@Moses-Chens-MBP.home>
2018-06-10 08:00:08 -0400

    keeping the contact object light, the game engine will load up profiles of the contact at random

--- src/Scripts/Objects/Contact.js
+++ src/Scripts/Objects/Contact.js
@@ -2,5 +2,4 @@ var Contact = function(args){
     this.name = args.hasOwnProperty("name") ? args.name : "";
     this.reachability = args.hasOwnProperty("reachability") ? args.reachability : 1;
     this.organization = args.hasOwnProperty("organization") ? args.organization : "";
-    this.responseLimit = Math.random(10 - reachability); 
 };
